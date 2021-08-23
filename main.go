34785f14ddadc18079d92767b3701e3946367dd2
bwhour <juvemeen.xio@gmail.com>
2021-08-23 11:41:10 +0800

    update some code

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/bwhour/sep31-demo/header"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(header.NoCache)
+	r.Use(header.Options)
+	r.Use(header.Secure)
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -15,9 +21,9 @@ func main() {
 	})
 
 	// add router.
-	r.GET("/info/", getInfo)
-	r.GET("/transaction", getTransaction)
-	r.POST("/transactions", createTransaction)
+	r.GET("/sep0031/info/", getInfo)
+	r.GET("/sep0031/transactions/:id", getTransaction)
+	r.POST("/sep0031/transactions", createTransaction)
 
 	r.Run(":9935") // listen and serve on 0.0.0.0:9935
 }
@@ -29,9 +35,10 @@ func getInfo(c *gin.Context) {
 }
 
 func getTransaction(c *gin.Context) {
-
+	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{
-		"data": "getTransaction",
+		"data": "getTransactions",
+		"id":   id,
 	})
 }
 
