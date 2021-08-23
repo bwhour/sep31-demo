a5e3bdee80c472b2eac88c2a982a2bea1b53a23f
bwhour <juvemeen.xio@gmail.com>
2021-08-23 14:14:55 +0800

    add response struct

--- main.go
+++ main.go
@@ -7,6 +7,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type info struct {
+	Amount      uint64 `json:"amount"`
+	Asset_code  string `json:"asset_code"`
+	Sender_id   string `json:"sender_id"`
+	Receiver_id string `json:"receiver_id"`
+	Field       map[string]string
+}
+
+type transaction struct {
+	Id                 string `json:"id"`
+	Stellar_account_id string `json:"stellar_account_id"`
+	Stellar_memo_type  string `json:"stellar_memo_type"`
+	Stellar_memo       string `json:"stellar_memo"`
+	Extra_info         map[string]string
+	How                string `json:"how"`
+}
+
+type transinfo struct {
+	Id                      string `json:"id"`
+	Status                  string `json:"status"`
+	Amount_in               string `json:"amount_in"`
+	Amount_out              string `json:"amount_out"`
+	Amount_fee              string `json:"amount_fee"`
+	Stellar_account_id      string `json:"stellar_account_id"`
+	Stellar_memo_type       string `json:"stellar_memo_type"`
+	Stellar_memo            string `json:"stellar_memo"`
+	Started_at              int64  `json:"timestamp"`
+	Stellar_transaction_id  string `json:"stellar_transaction_id"`
+	External_transaction_id string `json:"external_transaction_id"`
+}
+
 func main() {
 	r := gin.Default()
 
@@ -15,7 +46,7 @@ func main() {
 	r.Use(header.Secure)
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
