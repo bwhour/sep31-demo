b12e169fcc18f1f7b8275d4e850756be727f4fb1
bwhour <juvemeen.xio@gmail.com>
2021-08-24 21:09:42 +0800

    update info testdata

--- main.go
+++ main.go
@@ -30,9 +30,61 @@ func main() {
 }
 
 func getInfo(c *gin.Context) {
-
+    resp := map[string]interface{} {
+		    "receive":map[string]interface{} {
+			    "USDC":map[string]interface{} {
+				    "quotes_supported": true,
+				    "fee_fixed": 5,
+				    "fee_percent": 1,
+				    "min_amount": 0.1,
+				    "max_amount": 1000,
+				    "sep12": map[string]interface{}{
+					    "sender":map[string]interface{} {
+						    "types":map[string]interface{} {
+							    "sep31-sender": map[string]interface{}{
+								    "description": "U.S. citizens limited to sending payments of less than $10,000 in value ",
+							    },
+							    "sep31-large-sender": map[string]interface{}{
+								    "description": "U.S. citizens that do not have sending limits",
+							    },
+							    "sep31-foreign-sender": map[string]interface{}{
+								    "description": "non-U.S. citizens sending payments of less than $10,000 in value",
+							    },
+						    },
+					    },
+					    "receiver": map[string]interface{}{
+						    "types": map[string]interface{}{
+							    "sep31-receiver": map[string]interface{}{
+								    "description": "U.S. citizens receiving USD",
+							    },
+							    "sep31-foreign-receiver": map[string]interface{}{
+								    "description": "non-U.S. citizens receiving USD",
+							    },
+						    },
+					    },
+				    },
+				    "fields":map[string]interface{}{
+					    "transaction":map[string]interface{}{
+						    "receiver_routing_number":map[string]interface{}{
+							    "description": "routing number of the destination bank account",
+						    },
+						    "receiver_account_number":map[string]interface{}{
+							    "description": "bank account number of the destination",
+						    },
+						    "type":map[string]interface{}{
+							    "description": "type of deposit to make",
+							    "choices":[]string{"SEPA","SWIFT"},
+						    },
+					    },
+				    },
+			    },
+	    },
+	
+	
+    }
 	c.JSON(http.StatusOK, gin.H{
 		"method": "getInfo",
+		"resp":resp,
 	})
 }
 
