05659b41b9321f42b7016477355072e40217fe91
bwhour <juvemeen.xio@gmail.com>
2021-09-01 17:33:24 +0800

    update some code due to changed requirement

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/bwhour/sep31-demo/header"
 	"github.com/bwhour/sep31-demo/schema"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -82,33 +83,27 @@ func getInfo(c *gin.Context) {
 			},
 		},
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"method": "getInfo",
-		"resp":   resp,
-	})
+	c.JSON(http.StatusOK, resp)
 }
 
 func getTransaction(c *gin.Context) {
 	id := c.Param("id")
 	resp := []schema.Transinfo{
 		{
-			Id:                    "3bf73ba8-8a48-41b7-99da-b3d70ad959c0",
+			Id:                    id,
 			Status:                "completed",
 			AmountIn:              "53.0000000",
 			AmountOut:             "51.6066212",
 			AmountFee:             "0.0000000",
-			StellarAccountId:      "GBUQO65XW7TDXIHND7MFNIUZU6UNBA5XN27ODFDXVOLZYOXQSQUTPVHW",
+			StellarAccountId:      "GDRZFOASKJUV5E2ETWJY5RZRKQF3PMCZ2CNLLWH365X7OUM5NHUOVE7I",
 			StellarMemoType:       "text",
 			StellarMemo:           "psp:2850",
 			StartedAt:             "2020-10-06 14:50:40.508155 +0000 UTC",
 			StellarTransactionId:  "18b6a4b464b061aefd949c00645deb6732b18ed3acb723d3ba5ac6ba00465d94",
 			ExternalTransactionId: "18b6a4b464b061aefd949c00645deb6732b18ed3acb723d3ba5ac6ba00465d94",
-		}}
-	c.JSON(http.StatusOK, gin.H{
-		"method": "getTransactions",
-		"id":     id,
-		"resp":   resp,
-	})
+		},
+	}
+	c.JSON(http.StatusOK, resp)
 }
 
 func getCustomer(c *gin.Context) {
@@ -135,41 +130,32 @@ func getCustomer(c *gin.Context) {
 		},
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"method": "getCustomer",
-		"resp":   resp,
-	})
+	c.JSON(http.StatusOK, resp)
 }
+
 func updateCustomer(c *gin.Context) {
+
 	c.JSON(http.StatusOK, gin.H{
-		"method": "getCustomer",
-		"id":     "391fb415-c223-4608-b2f5-dd1e91e3a986",
+		"id": "391fb415-c223-4608-b2f5-dd1e91e3a986",
 	})
 }
 
 func createTransaction(c *gin.Context) {
+	id := uuid.New().String()
 	resp := []schema.Transaction{
 		{
-			Id:                 "6238dd28-a47a-470e-9cee-7f547730d6b1",
-			Stellar_account_id: "GBUQO65XW7TDXIHND7MFNIUZU6UNBA5XN27ODFDXVOLZYOXQSQUTPVHW",
+			Id:                 id,
+			Stellar_account_id: "GDRZFOASKJUV5E2ETWJY5RZRKQF3PMCZ2CNLLWH365X7OUM5NHUOVE7I",
 			Stellar_memo_type:  "text",
-			Stellar_memo:       "psp:2852",
+			Stellar_memo:       "psp:18000",
 			Extra_info: map[string]interface{}{
-				"amount":                  "53",
-				"bank_name":               "BRED BAGNOLET",
-				"beneficiary":             "TEMPO FRANCE",
-				"beneficiary_address":     "89 BOULEVARD DE MAGENTA 75010 PARIS",
-				"bic":                     "BREDFRPPXXX",
-				"currency":                "EUR",
-				"external_transaction_id": "5b22d1ed-b789-48e0-9d45-9d2f116f0159",
-				"iban":                    "FR76 1010 7002 3000 5220 3386 959",
-				"reference_id":            "swift_101",
-				"transaction_id":          "5b22d1ed-b789-48e0-9d45-9d2f116f0159",
+				"receiver_asset_code": "USDC",
+				"receiver_asset_amt":  "23.43",
+				"exchange_pair":       "USDC/THB",
+				"exchange_rate":       "32.931",
+				"buy_or_sell":         "sell",
 			},
-			How: "SWIFT",
-		}}
-	c.JSON(http.StatusOK, gin.H{
-		"method": "createTransaction",
-		"resp":   resp,
-	})
+		},
+	}
+	c.JSON(http.StatusOK, resp)
 }
